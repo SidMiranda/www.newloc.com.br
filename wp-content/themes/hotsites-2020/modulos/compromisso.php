f7c084b297c8b5f96835a9ffee9a670df0a1ab5b
Sidney Miranda <41812064+SidMiranda@users.noreply.github.com>
2023-05-01 13:58:22 -0300

    modulo paralax ok

--- wp-content/themes/hotsites-2020/modulos/compromisso.php
+++ wp-content/themes/hotsites-2020/modulos/compromisso.php
@@ -7,14 +7,15 @@
         $textoModuloCompromisso = get_field('textoModuloCompromisso', $post->ID);
 
 
-?>
+?>  <section id="paralax" style="background-image: url('<?php echo $bgCompromissoDesk ?>')"></section>
     <section id="compromisso">
         <div class="bg">
-            <img src="<?php echo $bgCompromissoMobile ?>" alt="Background compromisso" class="hidden-lg">
-            <img src="<?php echo $bgCompromissoDesk ?>" alt="Background compromisso" class="hidden-xs">
+            <!-- <img src="<?php //echo $bgCompromissoMobile ?>" alt="Background compromisso" class="hidden-lg">
+            <img src="<?php //echo $bgCompromissoDesk ?>" alt="Background compromisso" class="hidden-xs">-->
+						<span>COMPROMISSO COM A QUALIDADE</span>
         </div>
-        <div class="container">
-            <h2><?php echo $textoModuloCompromisso; ?></h2>
-        </div>
+        <!-- <div class="container">
+            <h2><?php //echo $textoModuloCompromisso; ?></h2>
+        </div> -->
     </section>
 <?php } ?>
--- wp-content/themes/hotsites-2020/modulos/sobre.php
+++ wp-content/themes/hotsites-2020/modulos/sobre.php
@@ -14,6 +14,15 @@
         $texto_solidarize = get_field('texto_solidarize', $post->ID);
         $iconeTitulo = get_field('iconeTitulo', $post->ID);
 
+				$background_mobile_sobre2 = get_field('background_mobile_sobre2', $post->ID);
+        $background_desk_sobre2 = get_field('background_desk_sobre2', $post->ID);
+        $faixas_desk2 = get_field('faixas_desk2', $post->ID);
+        $faixas_mobile2 = get_field('faixas_mobile2', $post->ID);
+        $titulo_sobre2 = get_field('titulo_sobre2', $post->ID);
+        $texto_sobre_o_evento2 = get_field('texto_sobre_o_evento2', $post->ID);
+        $texto_sobre_o_evento_mobile2 = get_field('texto_sobre_o_evento_mobile2', $post->ID);
+
+
 ?>
     <section id="sobreEvento">
         <div class="containerSobre">
@@ -35,17 +44,17 @@
         </div>
         <div class="containerSobre direita">
             <div class="container">
-                <h1 class="hidden-xs"><?php echo $titulo_sobre ?></h1>
+                <h1 class="hidden-xs"><?php echo $titulo_sobre2 ?></h1>
                 <div class="sobre">
-                    <img src="<?php echo $background_mobile_sobre ?>" class="hidden-lg" alt="">
+                    <img src="<?php echo $background_mobile_sobr2 ?>" class="hidden-lg" alt="">
                     <div class="imgs-bg">
-                        <img src="<?php echo $background_desk_sobre ?>" class="hidden-xs" alt="">
+                        <img src="<?php echo $background_desk_sobre2 ?>" class="hidden-xs" alt="">
                         <div class="boxLaranja hidden-xs"></div>
                     </div>
                     <div class="cardEvento">
-                        <h1 class="hidden-lg"><?php echo $titulo_sobre ?></h1>
-                        <span class="hidden-xs"><?php echo $texto_sobre_o_evento ?></span>
-                        <p class="hidden-lg"><?php echo $texto_sobre_o_evento_mobile ?></p>
+                        <h1 class="hidden-lg"><?php echo $titulo_sobre2 ?></h1>
+                        <span class="hidden-xs"><?php echo $texto_sobre_o_evento2 ?></span>
+                        <p class="hidden-lg"><?php echo $texto_sobre_o_evento_mobile2 ?></p>
                     </div>
                 </div>
             </div>
